66b3078e86ae2ed2f4f288ff408d1ab07fc105c9
Woogie95 <choisw950928@gmail.com>
2023-07-18 16:50:36 +0900

    feat: swagger 설정

--- src/main/java/com/example/weather/controller/DiaryController.java
+++ src/main/java/com/example/weather/controller/DiaryController.java
@@ -2,6 +2,8 @@ package com.example.weather.controller;
 
 import com.example.weather.entity.Diary;
 import com.example.weather.service.DiaryService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
@@ -15,20 +17,25 @@ public class DiaryController {
 
     private final DiaryService diaryService;
 
+    @ApiOperation("일기 텍스트와 날씨를 이용해서 DB 에 일기 저장")
     @PostMapping("/create/diary")
     public void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
                             @RequestBody String text) {
         diaryService.createDiary(date, text);
     }
 
+    @ApiOperation("선택한 날짜의 모든 일기 데이터를 가져옵니다.")
     @GetMapping("/read/diary")
     public List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         return diaryService.readDiary(date);
     }
 
+    @ApiOperation("선택한 기간중의 모든 일기 데이터를 가져옵니다.")
     @GetMapping("/read/diaries")
-    public List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-                                   @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+    public List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                                   @ApiParam(value = "조회할 기간의 시작한날", example = "2023-07-18") LocalDate startDate,
+                                   @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                                   @ApiParam(value = "조회할 기간의 마지막날", example = "2023-07-18") LocalDate endDate) {
         return diaryService.readDiaries(startDate, endDate);
     }
 
